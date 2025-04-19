e3b5d402ab4b7f7d7aa3ad4967ee61df13e57579
Vitalii Loiko <vloyko@mindk.com>
2025-04-19 20:32:23 +0300

    fix(mf/uikit): test commit

--- appbar/module-federation.config.ts
+++ appbar/module-federation.config.ts
@@ -1,9 +1,11 @@
 import { createModuleFederationConfig } from '@module-federation/rsbuild-plugin';
 
+const { PUBLIC_UIKIT_REMOTE_URL } = process.env;
+
 export default createModuleFederationConfig({
   name: 'appbar',
   remotes: {
-    uikit: 'uikit@http://localhost:3004/mf-manifest.json',
+    uikit: `uikit@${PUBLIC_UIKIT_REMOTE_URL}/mf-manifest.json`,
   },
   exposes: {
     '.': './src/AppBar',
